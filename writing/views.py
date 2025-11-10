8e15bcdedeacb7a3abceb948aa5a0a53b82d60c1
A1909bek <aa2004bek@gmail.com>
2025-11-11 00:16:41 +0500

    adding solution

--- writing/views.py
+++ writing/views.py
@@ -9,12 +9,6 @@ class WritingTaskAPIView(APIView):
     def get(self,request):
         writing_tasks = WritingTask.objects.all()
         serializer = WritingTaskSerializer(writing_tasks,many=True)
-        response = {
-            "task_type":serializer.task_type,
-            "title":serializer.title,
-            "image":serializer.image,
-            "question":serializer.question
-        }
-        return Response(response.data,status=status.HTTP_200_OK)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
 
