d7fc0c2b4642ac2b8b55e2b417ce9e665ab8e0fa
A1909bek <aa2004bek@gmail.com>
2025-12-08 10:23:39 +0500

    adding some changes

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from decouple import config
@@ -76,8 +76,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 ROOT_URLCONF = 'config.urls'
 
@@ -162,8 +160,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/app/staticfiles'   # muhim!
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media'
+
+# WhiteNoise (agar ishlatayotgan boʻlsangiz)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
